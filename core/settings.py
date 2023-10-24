ea51b876a6c6d9c6acce0a7fba479ee1814f94ef
Simoniel Musyoki <musyoki.mtk3@gmail.com>
2023-10-24 11:17:02 +0300

    Deployment Requirements

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = []
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
