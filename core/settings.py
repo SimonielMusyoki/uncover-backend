3a751317302ec848147fbb0a912a97ae555a2115
Simoniel Musyoki <musyoki.mtk3@gmail.com>
2023-10-24 12:28:48 +0300

    Allowed AWS Server IP

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,8 @@ MEDIA_ROOT = str(BASE_DIR / "mediafiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 CORS_ALLOWED_ORIGINS = [
+    'http://16.171.1.141:8000',
+    'http://16.171.1.141:4200',
     "http://localhost:8000",
     "http://localhost:4200",
     "http://127.0.0.1:8000",
@@ -150,6 +152,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://0.0.0.0:4200",
 ]
 CORS_TRUSTED_ORIGINS = [
+'http://16.171.1.141:8000',
+    'http://16.171.1.141:4200',
     "http://localhost:8000",
     "http://localhost:4200",
     "http://127.0.0.1:8000",
